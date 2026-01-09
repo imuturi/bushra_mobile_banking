4e35e84f7c6bfa7eae9a24d3f64cdc4d67c56023
gmulonga <gadsonmulonga@gmail.com>
2026-01-09 09:22:41 +0300

    fix: showing the full account number

--- lib/home/fund-transfers/page-home-transfer-v2-under-others.dart
+++ lib/home/fund-transfers/page-home-transfer-v2-under-others.dart
@@ -607,7 +607,9 @@ class _FundsTransferOtherScreenState extends State<FundsTransferOtherScreen> {
             icon: const Icon(Icons.arrow_drop_down),
             items: accounts?.map<DropdownMenuItem<String>>((account) {
               String maskedAccount =
-                  "A/C #${account.accountNumber.substring(0, 4)}****${account.accountNumber.substring(account.accountNumber.length - 4)}";
+                  "A/C #${account.accountNumber}";
+              // String maskedAccount =
+              //     "A/C #${account.accountNumber.substring(0, 4)}****${account.accountNumber.substring(account.accountNumber.length - 4)}";
               return DropdownMenuItem<String>(
                 value: account.accountNumber,
                 child: Text(maskedAccount, style: TextStyle(fontSize: 12),),
